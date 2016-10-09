1c9c748e08989a1579edf7502b0146defa2b569a
Denick Murray <denick@denick.org>
2016-10-09 22:18:09 +0200

    preg_replace was not applied.

--- web/mastermind.php
+++ web/mastermind.php
@@ -4,9 +4,10 @@ error_reporting(E_ALL);
   if(!empty( $_GET["player"]) && !empty( $_GET["guess"])){
     $player = $_GET["player"];
     $player = substr($player, 0, 10);
-    preg_replace('/[^a-zA-Z]/', '', $player);
+    $player = preg_replace('/[^a-zA-Z]/', '', $player);
+
     $guess = $_GET["guess"];
-    preg_replace('/[^1-6]/', '', $guess);
+    $guess = preg_replace('/[^1-6]/', '', $guess);
 
     $command = escapeshellcmd("../mastermind.py $player $guess");
     $output = shell_exec($command);
